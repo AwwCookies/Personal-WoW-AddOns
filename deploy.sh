472c411d22ee5345a17a5936fa460373ae25c8a5
AwwCookies <emma@emaw.dev>
2022-10-27 01:08:56 -0400

    added var wow_folder

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
+wow_folder="/mnt/w/World of Warcraft/"
 clients=("ptr" "retail" "beta")
 
 for client in ${clients[@]}
 do
-    cp -Rv ./* "/mnt/w/World of Warcraft/_"$client"_/Interface/AddOns"
-    rm -v "/mnt/w/World of Warcraft/_"$client"_/Interface/AddOns/deploy.sh"
+    cp -Rv ./* "$wow_folder""_"$client"_/Interface/AddOns"
+    rm -v "$wow_folder""_"$client"_/Interface/AddOns/deploy.sh"
 done
-
-# for client in ${clients[@]}
-# do
-    
-# done
