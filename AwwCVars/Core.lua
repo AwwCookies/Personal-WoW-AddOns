bcf646010a0a4432a37379f3b3f1c1051ebb4576
AwwCookies <emma@emaw.dev>
2022-10-30 11:50:00 -0400

    refactoring of code

--- AwwCVars/Core.lua
+++ AwwCVars/Core.lua
@@ -1,22 +1,18 @@
 local f = CreateFrame("Frame")
 
-f:SetScript("OnEvent", function() 
-    print("Enabling Interact Key")
-    -- press key to interact with stuff
-    SetCVar("SoftTargetInteract", "3")
-    -- action combat
-    print("Enabling Action Combat")
-    SetCVar("SoftTargetEnemy", "3")
-    -- show target tool tips without needing
-    -- to hard tab them
-    print("Enabling Show Target Tooltip")
-    SetCVar("SoftTargetTooltipEnemy", "1")
-    -- This will allow you to interact with the bobber no matter which way you are facing.
-    print("Setting TargetInteract Arc to 2")
-    SetCVar("SoftTargetInteractArc", "2")
-    -- Set interact range
-    print("Setting SoftTargetRange to 30")
-    SetCVar("SoftTargetInteractRange", "30")
+local cvars = {
+    ["SoftTargetInteract"] = "3",
+    ["SoftTargetEnemy"] = "3",
+    ["SoftTargetTooltipEnemy"] = "1",
+    ["SoftTargetInteractArc"] = "2",
+    ["SoftTargetInteractRange"] = "30",
+}
+
+f:SetScript("OnEvent", function()
+    for name, value in pairs(cvars) do
+        print("Setting CVar", name, " to ", value)
+        SetCVar(name, value)
+    end
 end)
 
 f:RegisterEvent("PLAYER_LOGIN")
