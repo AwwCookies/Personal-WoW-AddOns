6232a54561591090059dff225147c2cd8ac07f20
AwwCookies <emma@emaw.dev>
2022-10-30 11:12:46 -0400

    changed awwsbugfixes to AwwCVars and added new cvar sets

--- AwwCVars/Core.lua
+++ AwwCVars/Core.lua
@@ -11,6 +11,12 @@ f:SetScript("OnEvent", function()
     -- to hard tab them
     print("Enabling Show Target Tooltip")
     SetCVar("SoftTargetTooltipEnemy", "1")
+    -- This will allow you to interact with the bobber no matter which way you are facing.
+    print("Setting TargetInteract Arc to 2")
+    SetCVar("SoftTargetInteractArc", "2")
+    -- Set interact range
+    print("Setting SoftTargetRange to 30")
+    SetCVar("SoftTargetInteractRange", "30")
 end)
 
 f:RegisterEvent("PLAYER_LOGIN")
