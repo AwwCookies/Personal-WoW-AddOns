536a31ce290c33adeafced712cab04c7b85c23f9
AwwCookies <emma@emaw.dev>
2022-10-31 02:34:53 -0400

    added more CVars

--- AwwCVars/Core.lua
+++ AwwCVars/Core.lua
@@ -4,9 +4,14 @@ local cvars = {
     ["SoftTargetInteract"] = "3", -- enable keypress interaction
     ["SoftTargetEnemy"] = "3", -- enable action targeting
     ["SoftTargetTooltipEnemy"] = "1", -- Enable tooltips on mobs no mouseover
-    ["SoftTargetInteractArc"] = "2", -- ?
+    ["SoftTargetTooltipFriend"] = "1", -- enable tool tips on friendly no mouseover
+    ["SoftTargetInteractArc"] = "2", -- set yaw allowence to be anywhere in targeting area
     ["SoftTargetInteractRange"] = "30", -- set interaction range (may be removed)
+    ["SoftTargetIconInteract"] = "1", -- enable soft target icons
+    ["SoftTargetIconGameObject"] = "1", -- enable icons over game objects
     ["SoftTargetLowPriorityIcons"] = "1", -- enable loot icons 
+    ["empowerTapControls"] = "1", -- enable press and tap empowered spells
+    ["ActionButtonUseKeyHeldSpell"] = "1", -- enable press and hold casting
 }
 
 f:SetScript("OnEvent", function()
