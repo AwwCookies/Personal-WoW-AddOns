cbb7d4fdd6f3b934e2a36f88faefe5d56c8d3b00
AwwCookies <emma@emaw.dev>
2022-10-31 02:12:29 -0400

    added cvar for low priority icons

--- AwwCVars/Core.lua
+++ AwwCVars/Core.lua
@@ -6,6 +6,7 @@ local cvars = {
     ["SoftTargetTooltipEnemy"] = "1", -- Enable tooltips on mobs no mouseover
     ["SoftTargetInteractArc"] = "2", -- ?
     ["SoftTargetInteractRange"] = "30", -- set interaction range (may be removed)
+    ["SoftTargetLowPriorityIcons"] = "1", -- enable loot icons 
 }
 
 f:SetScript("OnEvent", function()
